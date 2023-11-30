a670039901dffdaa38c0310e3f0e1ffc4d519ace
Yash Arya <yasharya2901@gmail.com>
2023-11-30 13:25:41 +0530

    Added ability to search for the product or to enter their URL
    Added argument to the main function which will be the URL of the Product. However, the main is always called without any argument at first.

--- main.py
+++ main.py
@@ -2,48 +2,62 @@ from amazon_scrapper import *
 from flipkart_scrapper import *
 import sys
 
-def main():
-    url = input("Enter the URL: ")
-    if "amazon" in url:
-        product_amazon = amazon(url)
-        product_amazon.print_product_info()
-
-        print("Would you like to search for the product automatically on Flipkart?")
-        print("Press 'y' to continue. Or Enter the Flipkart URL")
-        response = input("> ")
-
-        if response == "y" or response == "Y":
-            flipkart_link = flipkart.search_item(product_amazon.name)
-            if (flipkart_link == "exit"):
-                sys.exit("Exiting...")
-        else:
-            flipkart_link = response
-        
-        product_flipkart = flipkart(flipkart_link)
-        product_flipkart.print_product_info()
-
-
-    elif "flipkart" in url:
-        product_flipkart = flipkart(url)
-        product_flipkart.print_product_info()
-
-        print("Would you like to search for the product automatically on Amazon?")
-        print("Press 'y' to continue. Or Enter the Amazon URL")
-        response = input("> ")
-
-        if response == "y" or response == "Y":
-            amazon_link = amazon.search_item(product_flipkart.name)
-            if (amazon_link == "exit"):
-                sys.exit("Exiting...")
-        else:
-            amazon_link = response
-
-        product_amazon = amazon(amazon_link)
-        product_amazon.print_product_info()
-        
+def main(*args):
+    if args:
+        response = args[0]
     else:
-        print("Website Not Supported")
-    
+        response = input("Enter the product name or URL: ")
+    if "http" in response:
+        url = response
+        if "amazon" in url:
+            product_amazon = amazon(url)
+            product_amazon.print_product_info()
+
+            print("Would you like to search for the product automatically on Flipkart?")
+            print("Press 'y' to continue. Or Enter the Flipkart URL")
+            response = input("> ")
+
+            if response == "y" or response == "Y":
+                flipkart_link = flipkart.search_item(product_amazon.name)
+                if (flipkart_link == "exit"):
+                    sys.exit("Exiting...")
+            else:
+                flipkart_link = response
+            
+            product_flipkart = flipkart(flipkart_link)
+            product_flipkart.print_product_info()
+
+
+        elif "flipkart" in url:
+            product_flipkart = flipkart(url)
+            product_flipkart.print_product_info()
+
+            print("Would you like to search for the product automatically on Amazon?")
+            print("Press 'y' to continue. Or Enter the Amazon URL")
+            response = input("> ")
+
+            if response == "y" or response == "Y":
+                amazon_link = amazon.search_item(product_flipkart.name)
+                if (amazon_link == "exit"):
+                    sys.exit("Exiting...")
+            else:
+                amazon_link = response
+
+            product_amazon = amazon(amazon_link)
+            product_amazon.print_product_info()
+            
+        else:
+            print("Website Not Supported")
+    else:
+        platform = input("Where would you like to search that product? Amazon or Flipkart? ")
+        if platform == "amazon" or platform == "Amazon":
+            main(amazon.search_item(response))
+
+        elif platform == "flipkart" or platform == "Flipkart":
+            main(flipkart.search_item(response))
+
+        else:
+            sys.exit("Platform not supported.")
     return 0
 
 if __name__ == '__main__':
